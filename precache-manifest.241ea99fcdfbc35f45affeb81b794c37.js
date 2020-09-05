e32fb49d8a3ca61400a01280f14ce02ae8e8146e
Nikita Sokolov <mr.sokolov99@gmail.com>
2020-09-05 12:49:37 +0400

    Updates

---
+++ precache-manifest.241ea99fcdfbc35f45affeb81b794c37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3d879e4388f573d793ea7d01a26087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b675fb3b93ee4c69c65",
+    "url": "/static/js/2.56bfd40e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.56bfd40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7f56a6de98b2b75ee8",
+    "url": "/static/js/main.c3166f7e.chunk.js"
+  },
+  {
+    "revision": "2e71319991222b421a36",
+    "url": "/static/js/runtime-main.22e28c68.js"
+  }
+]);
